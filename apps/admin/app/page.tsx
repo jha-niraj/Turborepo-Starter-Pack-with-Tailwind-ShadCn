128a1a15dfa86bdbf49fb6f34a806d79ad36f362
jha-niraj <jhaniraj45@gmail.com>
2025-12-19 02:21:55 +0530

    init: Initialize the turborepo with the tailwind and shadcn

---
+++ apps/admin/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@repo/ui/components/ui/button";
+
+export default function Home() {
+	return (
+		<div>
+			<Button>Admin Button</Button>
+			<h1 className="text-red-500">Niraj</h1>
+		</div>
+	);
+}
---
+++ apps/main/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@repo/ui/components/ui/button";
+
+export default function Home() {
+	return (
+		<div>
+			<Button>Main Button</Button>
+		</div>
+	);
+}
